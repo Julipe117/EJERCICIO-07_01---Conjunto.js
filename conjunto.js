fe826493984875d8976dd2a1ed8148639b9603c7
Julipe117 <110401271+Julipe117@users.noreply.github.com>
2022-09-09 11:25:14 +0200

    Add files via upload

---
+++ conjunto.js
@@ -0,0 +1,7 @@
+const familia = ["Juanma","Mari","Sandra","Sergio"];
+const miSet = new Set(familia);
+console.log(miSet);
+miSet.add("Sergio");
+console.log(miSet);
+miSet.add("Javascript");
+console.log(miSet);
